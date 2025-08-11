6df74418ddb9757a1463a56684e0901b6e2651d6
Kashif Sohail <kashif@mawqiftech.com>
2025-08-11 18:19:22 +0500

    fix api route issue

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Core\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
-    // api routes should be here
+    // You can define all apis routes here
 });
