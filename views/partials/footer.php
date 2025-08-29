e11979791a02165c9ca5fb3978dabb443e4a565a
Kashif Sohail <kashif@mawqiftech.com>
2025-08-29 12:41:23 +0500

    fix footer name

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer>
     <div>Created By <strong>M. Kashif Sohail</strong> © 2025 |
-        <a href="#">Larite.com</a>
+        <a href="https://larite.dev/">Larite.dev</a>
     </div>
 </footer>
 <!-- Footer -->
