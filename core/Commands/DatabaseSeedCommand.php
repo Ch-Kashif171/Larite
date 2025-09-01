88c176984ee162cbfb7ada808e165a778873a14f
Kashif Sohail <kashif@mawqiftech.com>
2025-09-01 13:30:40 +0500

    db seeder command to run specific seeder

--- core/Commands/DatabaseSeedCommand.php
+++ core/Commands/DatabaseSeedCommand.php
@@ -4,6 +4,8 @@ namespace Core\Commands;
 
 use Database\Seeders\DatabaseSeeder;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DatabaseSeedCommand extends Command
@@ -12,19 +14,44 @@ class DatabaseSeedCommand extends Command
     {
         $this
             ->setName('db:seed')
-            ->setDescription('Seed the database using DatabaseSeeder class.');
+            ->setDescription('Seed the database using DatabaseSeeder class or a specific seeder.')
+            ->addOption(
+                'class',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The name of the seeder class to run'
+            );
     }
 
-    protected function execute($input, OutputInterface $output): int
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-
         try {
+            $class = $input->getOption('class');
 
-            $seeder = new DatabaseSeeder();
+            if ($class) {
+                // If the user didn't provide a full namespace, prefix it
+                if (!str_contains($class, '\\')) {
+                    $class = "Database\\Seeders\\{$class}";
+                }
 
-            $seeder->run();
+                $output->writeln("<info>Seeding with {$class}...</info>");
 
-            $output->writeln('<info>Database seeding completed successfully.</info>');
+                if (!class_exists($class)) {
+                    $output->writeln("<error>Seeder class {$class} not found.</error>");
+                    return Command::FAILURE;
+                }
+
+                $seeder = new $class();
+                $seeder->run();
+
+                $output->writeln("<info>{$class} completed successfully.</info>");
+            } else {
+                // Run all seeders via DatabaseSeeder
+                $seeder = new DatabaseSeeder();
+                $seeder->run();
+
+                $output->writeln('<info>Database seeding completed successfully.</info>');
+            }
         } catch (\Throwable $e) {
             $output->writeln('<error>Error while seeding: ' . $e->getMessage() . '</error>');
             return Command::FAILURE;
